b06dc67d4382d36c833b13aa5e3038c186ebc168
Daniel Schroeder <schrsistemas@gmail.com>
2026-03-30 20:47:55 -0300

    feat: add main application class

---
+++ backend/src/main/java/com/dataguard/erp/DataGuardApplication.java
@@ -0,0 +1,11 @@
+package com.dataguard.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGuardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataGuardApplication.class, args);
+    }
+}
